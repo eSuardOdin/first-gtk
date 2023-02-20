6162755183f9a7187f236d502bd650b0cff21e30
eSuardOdin <esuardodin@gmail.com>
2023-02-20 20:36:13 +0100

    Grid test

--- test.c
+++ test.c
@@ -25,21 +25,22 @@ int main (int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);                      // Creating window
     
-    char btnName[255] = "Close window";
+    
     GtkWidget *lbl = gtk_label_new ("My Label");
-    GtkWidget *closeBtn = gtk_button_new_with_label (btnName);                  // Create a close btn
+    GtkWidget *closeBtn = gtk_button_new_with_label ("Close window");                  // Create a close btn
     GtkWidget *countBtn = gtk_button_new_with_label ("Add");                    // Count btn
     g_signal_connect (countBtn, "clicked", G_CALLBACK (count_button), lbl); 
     g_signal_connect (closeBtn, "clicked", G_CALLBACK (end_program), NULL);     // End program on btn click
     g_signal_connect (win, "delete_event", G_CALLBACK (end_program), NULL);     // End program on cross click
     
     
-    
+    /*
     GtkWidget *box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);                 // https://docs.gtk.org/gtk3/class.Box.html
-	gtk_box_pack_start (GTK_BOX (box), lbl, TRUE, TRUE, 0);				        // Append all to box
-	gtk_box_pack_start (GTK_BOX (box), countBtn, TRUE, TRUE, 0);
-	gtk_box_pack_start (GTK_BOX (box), closeBtn, TRUE, TRUE, 0);
-    
+    gtk_box_pack_start (GTK_BOX (box), countBtn, TRUE, FALSE, 0);               // Append all to box 
+	gtk_box_pack_start (GTK_BOX (box), lbl, TRUE, FALSE, 0);				        
+	
+	gtk_box_pack_start (GTK_BOX (box), closeBtn, TRUE, FALSE, 0);
+    */
     /*
     // Test args
     if(argc > 1) {
@@ -50,7 +51,12 @@ int main (int argc, char *argv[]) {
 	}
     */
     
-    gtk_container_add (GTK_CONTAINER (win), box);                          // Append box to window
+    GtkWidget *grid = gtk_grid_new ();
+    gtk_grid_attach(GTK_GRID(grid),lbl, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid),countBtn, 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid),closeBtn, 0, 1, 2, 1); 
+    
+    gtk_container_add (GTK_CONTAINER (win), grid);                          // Append box to window
     
     gtk_widget_show_all (win);
     gtk_main ();
